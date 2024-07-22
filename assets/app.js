1438ddcc4d18b04c4c1caeafcd629e557d877fef
fehizoroRak <tsimbina@hotmail.fr>
2024-07-22 20:36:38 +0200

    Initial commit for autorent

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,13 @@
-import './bootstrap.js';
+// Import de VUE JS
+import { createApp } from 'vue';
+import App from './js/components/App.vue';
+createApp(App).mount('#app');
+
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * This file sera inclus sur la page via la fonction importmap() Twig,
+ * qui devrait déjà être dans votre base.html.twig.
  */
 import './styles/app.css';
 
---
+++ assets/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1 class="h1">Hello Vue!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  };
+  </script>
+  
+  <style>
+  .h1{
+    color: yellowgreen;
+    font-size: 3rem;
+  }
+  </style>
+  
