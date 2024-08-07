a50242f6c2c776b181d73195c7dce1be9bb476da
fehizoroRak <tsimbina@hotmail.fr>
2024-08-07 17:34:49 +0200

    work on pack and options pages 2

--- src/DataFixtures/OptionFixtures.php
+++ src/DataFixtures/OptionFixtures.php
@@ -15,25 +15,25 @@ class OptionFixtures extends Fixture
                 'name' => 'Conducteur additionnel',
                 'price' => 25.99,
                 'content' => 'Ajoutez un conducteur supplémentaire à votre contrat de location.',
-                'image' => 'conducteur_additionnel.png'
+                'image' => 'conducteur+.jpg'
             ],
             [
                 'name' => 'Siège de sécurité bébé (0-12 mois)',
                 'price' => 28.01,
                 'content' => 'Assurez la sécurité de votre bébé avec notre siège de sécurité.',
-                'image' => 'siege_bebe.png'
+                'image' => 'rehausseur0.jpg'
             ],
             [
                 'name' => 'Système de navigation GPS',
                 'price' => 40.01,
                 'content' => 'Trouvez facilement votre chemin avec notre GPS intégré.',
-                'image' => 'gps.png'
+                'image' => 'gps.jpg'
             ],
             [
                 'name' => 'Rehausseur enfant (4-7 ans)',
                 'price' => 18.00,
-                'content' => 'Assurez la sécurité de votre enfant avec notre rehausseur.',
-                'image' => 'rehausseur_enfant.png'
+                'content' => 'Assurez la sécurité de votre enfant (4 à 7 ans ou 15 à 30 kg) avec notre rehausseur.',
+                'image' => 'rehausseur7.jpg'
             ],
         ];
 
